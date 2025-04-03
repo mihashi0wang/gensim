de88b40bcfc0a7acda2c95b8b75c43567fe34b81
stc6895 <stc6895@aliyun.com>
2025-04-03 08:11:17 +0800

    fix build error

--- setup.py
+++ setup.py
@@ -341,7 +341,7 @@ install_requires = [
 
 setup(
     name='gensim',
-    version='4.3.3.1',
+    version='4.3.3',
     description='Python framework for fast Vector Space Modelling',
     long_description=LONG_DESCRIPTION,
 
